70add9ff9f85802fe92d439a7ec1bad3b7db542b
Anabel <anabeltale@gmail.com>
2023-05-28 11:48:37 -0300

    Add, funciones, trigger, vista y procedimientos
    se agregan Funciones, trigger, vistas y procedimientos al Creacion Tablas

--- Script_Creacion_Tablas.sql
+++ Script_Creacion_Tablas.sql
@@ -118,3 +118,113 @@ ALTER TABLE historiaClinicaDet ADD CONSTRAINT FK_historiaClinicaDet_historiaClin
     
 ALTER TABLE historiaClinicaDet ADD CONSTRAINT FK_historiaClinicaDet_profesionales FOREIGN KEY FK_historiaClinicaDet_profesionales (pro_codigo)
     REFERENCES profesionales (pro_codigo);
+
+   --FUNCIONES--
+    
+    DELIMITER //
+CREATE FUNCTION concatenar_nombre_apellido(nombre VARCHAR(255), apellido VARCHAR(255))
+RETURNS VARCHAR(510)
+BEGIN
+    DECLARE nombre_apellido VARCHAR(510);
+    SET nombre_apellido = CONCAT(nombre, ' ', apellido);
+    RETURN nombre_apellido;
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE FUNCTION concatenar_diagnosticos(dia_denom1 VARCHAR(255), dia_denom2 VARCHAR(255)  , dia_denom3 VARCHAR(255) , dia_denom4 VARCHAR(255) , dia_denom5 VARCHAR(255) , dia_denom6 VARCHAR(255) )
+RETURNS VARCHAR(2048)
+BEGIN
+    DECLARE resultado VARCHAR(2048);
+    SET resultado = '';
+    IF dia_denom1 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom1);
+    END IF;
+    IF dia_denom2 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom2);
+    END IF;
+    IF dia_denom3 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom3);
+    END IF;
+    IF dia_denom4 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom4);
+    END IF;
+    IF dia_denom5 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom5);
+    END IF;
+    IF dia_denom6 IS NOT NULL THEN
+        SET resultado = CONCAT(resultado, ' ', dia_denom6);
+    END IF;
+    RETURN TRIM(resultado);
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE sp_actualizar_paciente(IN p_codigo INT,
+										IN p_nombre VARCHAR(70),
+										IN p_apellido VARCHAR(70), 
+                                        IN p_dni VARCHAR(30), 
+                                        IN p_cud VARCHAR(254),
+                                        IN p_cud_img VARCHAR(254),
+                                        IN p_telefono VARCHAR(100),
+                                        IN p_direccion VARCHAR(254) )
+BEGIN
+    UPDATE paciente SET pac_nombre = p_nombre, 
+                        pac_apellido = p_apellido, 
+                        pac_dni = p_dni, 
+                        pac_cud = p_cud, 
+                        pac_cud_img = p_cud_img, 
+                        pac_telefono = p_telefono, 
+                        pac_direccion = p_direccion
+				  WHERE pac_codigo = p_codigo;
+END//
+DELIMITER ;
+
+
+
+DELIMITER //
+CREATE PROCEDURE ordenamiento_paciente (IN p_campo_a_ordenar varchar (200),IN orden BOOLEAN)
+BEGIN
+	IF p_campo_a_ordenar <> "" AND orden = 1 THEN
+		SET @ordenar = concat("ORDER BY", p_campo_a_ordenar);
+	ELSEIF p_campo_a_ordenar <> "" AND orden = 0 THEN
+		SET @ordenar = contact ("ORDER BY" , p_campo_a_ordenar, 'DESC');
+	ELSE
+		SET @ordenar = " "; 
+	END IF;
+	SET @clausula_select = concat ( " SELECT * FROM PACIENTES " , @ordenar);
+    
+	PREPARE ejecucion FROM @clausula_select;
+	EXECUTE ejecution;
+	DEALLOCATE PREPARE ejecucion;
+END //
+DELIMITER ;
+
+
+
+-- Crear tabla historiaClinica_log
+CREATE TABLE IF NOT EXISTS historiaClinica_log (
+    hcl_id INT AUTO_INCREMENT PRIMARY KEY,
+    hc_id INT,
+    hc_fecha_modificacion TIMESTAMP,
+    hc_campo_modificado VARCHAR(255),
+    hc_valor_anterior VARCHAR(255),
+    hc_valor_nuevo VARCHAR(255)
+);
+
+-- Crear trigger para guardar registros en historiaClinica_log
+DELIMITER //
+CREATE TRIGGER tr_historiaClinica_modificacion
+AFTER UPDATE
+ON historiaClinica FOR EACH ROW
+BEGIN
+    -- Insertar en tabla historiaClinica_log
+    INSERT INTO historiaClinica_log (hc_id, hc_fecha_modificacion, hc_campo_modificado, hc_valor_anterior, hc_valor_nuevo)
+    VALUES (OLD.id, CURRENT_TIMESTAMP(), 'campo_modificado', OLD.campo_modificado, NEW.campo_modificado);
+END;
+//
+DELIMITER ;
+
+-- Este trigger sirve para guardar registro de modificacion de la tabla HistoriaClinica
